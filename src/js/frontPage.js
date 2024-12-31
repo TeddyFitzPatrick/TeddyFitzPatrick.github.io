37499d28126d65c3dadb34cc61a5be70b8ff50c8
TeddyFitzPatrick <tfitz200@gmail.com>
2024-12-31 15:09:06 -0500

    fixed mobile view issues for project videos and updated UI colors

--- src/js/frontPage.js
+++ src/js/frontPage.js
@@ -1,26 +1,37 @@
 // Global vars
-let index, par, titleInterval
+let titleIndex, demoTitleIndex, demoTextIndex;
+let titleObject, demoTitleObject, demoTextObject;
+let titleInterval, demoTitleInterval, demoTextInterval;
 // Consts
-const titleStr = "Hi, I'm Teddy" 
-
+const titleStr = "Hi, I'm Teddy";
+const demoTitleStr = "* NEW *";
+const demoTextStr = " { Particle Life Demo }";
 
 
 window.onload = function () {
-    par = document.getElementById("title");
-    index = 0;
-
-
-    titleInterval = setInterval(addLetter, 150);
+    // Get the DOM objects
+    titleObject = document.getElementById("title");
+    demoTitleObject = document.getElementById("demo_title");
+    demoTextObject = document.getElementById("demo_text");
+    // Set the indices
+    titleIndex = {value: 0};
+    demoTitleIndex = {value: 0};
+    demoTextIndex = {value: 0};
+    // Set the intervals
+    titleInterval = setInterval(addLetter, 150, titleObject, titleStr, titleIndex);
+    demoTitleInterval = setInterval(addLetter, 150, demoTitleObject, demoTitleStr, demoTitleIndex);
+    demoTextInterval = setInterval(addLetter, 150, demoTextObject, demoTextStr, demoTextIndex);
 
 }
 
-
-function addLetter(){
-    // Stop the interval
-    if (index >= titleStr.length){
-        clearInterval(titleInterval);
+function addLetter(element, string, index){
+    console.log("?" + element.textContent);
+    if (index.value >= string.length){
         return;
+        clearInterval(titleInterval);
+        clearInterval(demoTitleInterval);
+        clearInterval(demoTextInterval);
     }
     // Add a character from the title string
-    par.textContent = par.textContent + titleStr[index++];
+    element.textContent += string[index.value++];
 }
