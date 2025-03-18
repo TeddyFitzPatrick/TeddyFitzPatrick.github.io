e308a92c132729b099d71dddb7db35d48178313d
TeddyFitzPatrick <tfitz200@gmail.com>
2025-03-17 20:28:12 -0400

    fixed title card mobile sizing and edited project description

--- src/js/frontPage.js
+++ src/js/frontPage.js
@@ -1,36 +1,28 @@
 // Global vars
-let titleIndex, demoTitleIndex, demoTextIndex;
-let titleObject, demoTitleObject, demoTextObject;
-let titleInterval, demoTitleInterval, demoTextInterval;
+let titleIndex, demoIndex;
+let titleObject, demoObject;
+let titleInterval, demoInterval
 // Consts
 const titleStr = "Hi, I'm Teddy";
-const demoTitleStr = "* NEW *";
-const demoTextStr = " { Particle Life Demo }";
+const demoStr = " { Particle Life Demo }";
 
 
 window.onload = function () {
     // Get the DOM objects
     titleObject = document.getElementById("title");
-    demoTitleObject = document.getElementById("demo_title");
-    demoTextObject = document.getElementById("demo_text");
+    demoObject = document.getElementById("demo_text");
     // Set the indices
     titleIndex = {value: 0};
-    demoTitleIndex = {value: 0};
-    demoTextIndex = {value: 0};
+    demoIndex = {value: 0};
     // Set the intervals
     titleInterval = setInterval(addLetter, 150, titleObject, titleStr, titleIndex);
-    demoTitleInterval = setInterval(addLetter, 150, demoTitleObject, demoTitleStr, demoTitleIndex);
-    demoTextInterval = setInterval(addLetter, 150, demoTextObject, demoTextStr, demoTextIndex);
+    demoInterval = setInterval(addLetter, 150, demoObject, demoStr, demoIndex);
 
 }
 
 function addLetter(element, string, index){
-    console.log("?" + element.textContent);
     if (index.value >= string.length){
         return;
-        clearInterval(titleInterval);
-        clearInterval(demoTitleInterval);
-        clearInterval(demoTextInterval);
     }
     // Add a character from the title string
     element.textContent += string[index.value++];
